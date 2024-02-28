35154e96291e7d9a9bb6e84b4a7cd7ac67bb56c4
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2024-02-28 13:22:57 +0600

    page degain

--- vue-breeze-api/src/components/Home.vue
+++ vue-breeze-api/src/components/Home.vue
@@ -5,6 +5,14 @@
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+const user = ref();
+onMounted (async () => {
+    const data = await axios.get('/api/user');
+    console.log(data);
+})
 
 </script>
 
