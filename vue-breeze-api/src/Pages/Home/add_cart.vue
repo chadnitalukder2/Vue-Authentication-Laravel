f37638c8df771ba63971bc7f14d87cedd0d221e2
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2024-03-11 21:15:58 +0600

    add cart page com

--- vue-breeze-api/src/Pages/Home/add_cart.vue
+++ vue-breeze-api/src/Pages/Home/add_cart.vue
@@ -4,6 +4,7 @@
 
 <template>
     <div class="container">
+        <div class="table" style="padding-bottom: 85px;">
         <table>
             <tr>
                 <th>Image</th>
@@ -46,6 +47,29 @@
                 <td>$100</td>
             </tr>
         </table>
+    </div>
+
+        <div class="cart_total">
+            <h3>Cart Totals</h3>
+            <p class="d-flex">
+                <span>Subtotal</span>
+                <span>$20.60</span>
+            </p>
+            <p class="d-flex">
+                <span>Delivery</span>
+                <span>$0.00</span>
+            </p>
+            <p >
+                <span>Discount</span>
+                <span>$3.00</span>
+            </p>
+            <hr style="background: rgba(255, 255, 255, 0.1);">
+    		<p >
+                <span>TOTAL</span>
+                <span style="    color: #000000;font-weight: 600;">$17.60</span>
+            </p>
+        </div>
+
 
     </div>
 </template>
@@ -56,6 +80,38 @@
     width: 100%;
     padding: 80px 100px;
     text-align: center;
+}
+.cart_total{
+    padding: 20px;
+    width: 30%;
+    text-align: center;
+    border: 1px solid rgba(0, 0, 0, 0.05);
+    h3{
+        font-size: 18px;
+        text-align: left;
+        text-transform: uppercase;
+        letter-spacing: 2px;
+        margin-bottom: 20px;
+        font-weight: 700;
+    }
+    p{
+        display: flex;
+        justify-content: space-between;
+        span{
+            font-family: "Open Sans", Arial, sans-serif;
+            font-size: 15px;
+            line-height: 1.8;
+            font-weight: 400;
+            color: gray;
+        }
+    }
+    hr{
+        background:rgba(255, 255, 255, 0.1);
+        margin-top: 1rem;
+        margin-bottom: 1rem;
+        border: 0;
+        border-top: 1px solid rgba(0, 0, 0, 0.1);
+    }
 }
 
 table {
@@ -71,6 +127,6 @@ th{
 th, td {
   padding: 15px;
   text-align: center;
-  border-bottom: 1px solid #ddd;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.05);
 }
 </style>
